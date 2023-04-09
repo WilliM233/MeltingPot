9e3d273250dc6354de791d4685ca25a1d22421af
WilliM233 <30969699+WilliM233@users.noreply.github.com>
2023-04-09 10:53:14 -0500

    Setup Premake to build files

---
+++ premake5.lua
@@ -0,0 +1,106 @@
+workspace "MeltingPot"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+	outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+	project "MeltingPot"
+		location "MeltingPot"
+		kind "SharedLib"
+		language "C++"
+
+		targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+		objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+		files
+		{
+			"%{prj.name}/src/**.h",
+			"%{prj.name}/src/**.cpp",
+		}
+
+		includedirs
+		{
+			"%{prj.name}/vendor/spdlog/include"
+		}
+
+		filter "system:windows"
+			cppdialect "C++17"
+			staticruntime "On"
+			systemversion "latest"
+
+			defines
+			{
+				"MP_PLATFORM_WINDOWS",
+				"MP_BUILD_DLL"
+			}
+
+			postbuildcommands
+			{
+				("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+			}
+
+		filter "configurations:Debug"
+			defines "MP_DEBUG"
+			symbols "On"
+
+		filter "configurations:Release"
+			defines "MP_RELEASE"
+			optimize "On"
+
+		filter "configurations:Dist"
+			defines "MP_DIST"
+			optimize "On"	
+			
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+		language "C++"
+
+		targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+		objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+		files
+		{
+			"%{prj.name}/src/**.h",
+			"%{prj.name}/src/**.cpp",
+		}
+
+		includedirs
+		{
+			"MeltingPot/vendor/spdlog/include",
+			"MeltingPot/src"
+		}
+
+		links
+		{
+			"MeltingPot"
+		}
+
+		filter "system:windows"
+			cppdialect "C++17"
+			staticruntime "On"
+			systemversion "latest"
+
+			defines
+			{
+				"MP_PLATFORM_WINDOWS",
+			}
+
+		filter "configurations:Debug"
+			defines "MP_DEBUG"
+			symbols "On"
+
+		filter "configurations:Release"
+			defines "MP_RELEASE"
+			optimize "On"
+
+		filter "configurations:Dist"
+			defines "MP_DIST"
+			optimize "On"	
